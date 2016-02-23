a37f4e85b93c5cc319b6ea545d5844c500008074
NONONOexe <g.kei0429@gmail.com>
2016-02-24 00:18:48 +0900

    実行ファイルの書き換え

--- AIWolfGameStarter.sh
+++ AIWolfGameStarter.sh
@@ -1,1 +1,1 @@
-java -cp lib/aiwolf-starter.jar:lib/aiwolf-server.jar:agent/* jp.ac.maslab.ando.aiwolf.starter.AIWolfGameStarter $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -cp lib/aiwolf-client.jar:lib/aiwolf-common.jar:lib/aiwolf-server.jar:lib/aiwolf-starter.jar:lib/aiwolf-viewer.jar:lib/jsonic-1.3.10.jar:agent/* jp.ac.maslab.ando.aiwolf.starter.AIWolfGameStarter $1 $2 $3 $4 $5 $6 $7 $8 $9
