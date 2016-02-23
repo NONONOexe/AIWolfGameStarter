7923e10ddb550ed5ff4193a7efcfd48536f18cd6
NONONOexe <g.kei0429@gmail.com>
2016-02-24 08:18:33 +0900

    README.mdの書き変え

--- AIWolfGameStarter.bat
+++ AIWolfGameStarter.bat
@@ -1,4 +1,3 @@
 @echo off
 cd /d %~dp0
-rem 人狼知能スターター起動
 java -cp lib/aiwolf-client.jar;lib/aiwolf-common.jar;lib/aiwolf-server.jar;lib/aiwolf-starter.jar;lib/aiwolf-viewer.jar;lib/jsonic-1.3.10.jar;agent/* jp.ac.maslab.ando.aiwolf.starter.AIWolfGameStarter %1 %2 %3 %4 %5 %6 %7 %8 %9
